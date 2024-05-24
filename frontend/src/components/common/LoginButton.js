50438853a0c4f70d1e5fc1334423271782990303
Janco Engelbrecht <engelbrechtjanco@gmail.com>
2024-05-24 10:13:48 +0200

    Added a Dropdown Menu

--- frontend/src/components/common/LoginButton.js
+++ frontend/src/components/common/LoginButton.js
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-import AuthProvider from '../../services/AuthProvider';
 
 const LoginButton = () => {
   const {loginWithRedirect} = useAuth0();
--- frontend/src/components/layout/navbar/index.js
+++ frontend/src/components/layout/navbar/index.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa";
 import LoginButton from "../../common/LoginButton";
 
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
     return (
       <div className="py-4 px-6 bg-gray-800 text-white relative z-50">
       <div className="flex justify-between items-center">
         <div className="text-xl font-bold">Logo</div> 
-        <div className="flex space-x-4">
-          <NavLink to="/" className="hover:text-gray-300">Home</NavLink>
-          <NavLink to="/contact" className="hover:text-gray-300">Contact Us</NavLink>
-          <NavLink to="/products" className="hover:text-gray-300">Products</NavLink>
-          <LoginButton />
+        <div className="relative">
+          <button onClick={toggleDropdown} className="hover:text-gray-300 focus:outline-none"><FaBars /></button>
+          {dropdownOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white text-black shadow-lg rounded">
+              <NavLink to="/" className="block px-4 py-2 hover:bg-gray-200" onClick={closeDropdown}>Home</NavLink>
+              <NavLink to="/contact" className="block px-4 py-2 hover:bg-gray-200" onClick={closeDropdown}>Contact Us</NavLink>
+              <NavLink to="/products" className="block px-4 py-2 hover:bg-gray-200" onClick={closeDropdown}>Products</NavLink>
+              <NavLink className="block px-4 py-2 hover:bg-gray-200" onClick={closeDropdown}><LoginButton /></NavLink>
+            </div>)
+          }
+          
         </div>
       </div>
     </div>
