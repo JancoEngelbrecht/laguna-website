74eccee21ba7cfc48e90d207ca802b2eaace84b1
Janco Engelbrecht <engelbrechtjanco@gmail.com>
2024-07-31 21:19:07 +0200

    Update Private Routes - Best Practice Code     - Add spinner component for loading     - Add default Value as an argument
    This update is part for the effort to clean up the code and implement best practices

--- frontend/src/routes/PrivateRoutes.js
+++ frontend/src/routes/PrivateRoutes.js
@@ -2,27 +2,31 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useRoles } from '../services/RoleProvider';
+import Spinner from '../components/common/global/Spinner';
 
-const PrivateRoute = ({ roles: requiredRoles }) => {
+const PrivateRoute = ({ roles: requiredRoles = [] }) => {
   const { isAuthenticated, isLoading } = useAuth0();
   const { roles: userRoles } = useRoles();
 
+  // Show a spinner while loading authentication status
   if (isLoading) {
-    return <div>Loading...</div>; // or a spinner component
+    return <Spinner />;
   }
 
+  // Redirect to home if the user is not authenticated
   if (!isAuthenticated) {
     return <Navigate to="/" />;
   }
 
   // Check if no specific roles are required or if the user has the required roles
-  const hasRequiredRole = requiredRoles === "" || requiredRoles.some(role => userRoles.includes(role));
+  const hasRequiredRole = requiredRoles.length === 0 || requiredRoles.some(role => userRoles.includes(role));
   
+  // Redirect to home if the user does not have the required roles
   if (!hasRequiredRole) {
     return <Navigate to="/" />;
   }
 
-  
+  // Render the child components if all checks pass
   return <Outlet />;
 };
 
