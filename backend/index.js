e4543ef30e5e6a948a08ed04599c0c5e082be246
Janco Engelbrecht <engelbrechtjanco@gmail.com>
2024-09-08 20:54:45 +0200

    Test Workflow #2

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv/config'); 
 
 const app = express();
+
 // Set up body parser middleware
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
